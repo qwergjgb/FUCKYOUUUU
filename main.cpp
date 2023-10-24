ba01c68547a23cabb773f9527d3bbc176eecc68d
evil <stukalovadana20@gmail.com>
2023-10-24 19:47:21 +0300

    Добавлен файл main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <windows.h>
 #include "functions.h"
-
+#include "functions.cpp"
 using namespace std;
 
 int main() {
-    int num1, num2;
-    char op;
-
+ int num1, num2;
+char op;
+  SetConsoleOutputCP(1251);
+  SetConsoleCP(1251);
     cout << "¬ведите первое число: ";
     cin >> num1;
     cout << "¬ведите второе число: ";
     cin >> num2;
     cout << "¬ведите оператор (+, -, *, /): ";
     cin >> op;
-
-    cout << "–езультат: " << calculate(num1, num2, op) << endl;
+  cout << "–езультат: " << calculate(num1, num2, op)<< endl;
 
     return 0;
 }
