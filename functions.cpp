ce53d5328d6bf17fdab5bf2bf665305d11edf1d3
evil <stukalovadana20@gmail.com>
2023-10-24 19:26:58 +0300

    Добавлен файл functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+#include "functions.h"
+
+int calculate(int num1, int num2, char op) {
+    int result;
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 0;
+            break;
+    }
+
+    return result;
+}
